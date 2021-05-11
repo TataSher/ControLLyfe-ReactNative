6659198cc898670d93cc2adbc095a4d68b899480
Emmapr123 <emmapriester@hotmail.com>
2021-05-11 17:57:10 +0100

    Styled add workout screen up to some point

--- src/screens/AddNewWorkoutScreen/AddNewWorkoutScreen.js
+++ src/screens/AddNewWorkoutScreen/AddNewWorkoutScreen.js
@@ -1,26 +1,79 @@
 
-import React from 'react';
-import { Button, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, KeyboardAvoidingView, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 const AddNewWorkoutScreen = () => {
 
+  const [workoutTitle, setWorkoutTitle] = useState('')
+  const [excersizeTitle, setExcersizeTitle] = useState([])
+  const [excersizeDescription, setExcersizeDesription] = useState([])
+  const [excersizeMin, setExcersizeMin] = useState([])
+  const [excersizeSec, setExcersizeSec] = useState([])
+  const [excersize, setExcersize] = useState([])
+
   return(
-    <ScrollView style={styles.bruh}>
-      <Text> Successs </Text>
-      <Text style={styles.bottom}> At the bottom </Text>
-    </ScrollView>
+    <KeyboardAvoidingView>
+      <TextInput
+        placeholder="Workout title"
+        style={styles.textInput}
+        onChangeText={(text) => setWorkoutTitle(text)}
+        value={workoutTitle}
+      />
+      <Text style={{fontSize: 24, margin: 8}}> Exercises </Text>
+      <View style={styles.excersizeInput}>
+      <TextInput
+      placeholder="Exercise"
+      onChangeText={(text) => setExcersizeTitle(text)}
+      value={excersizeTitle}
+      />
+      <TextInput
+      placeholder="Min"
+      onChangeText={(number) => setExcersizeMin(number)}
+      value={excersizeMin}
+      />
+      <TextInput
+      placeholder="Sec"
+      onChangeText={(number) => setExcersizeSec(number)}
+      value={excersizeSec}
+      />
+      </View>
+      <TextInput
+      placeholder='description'
+      style={styles.textInput}
+      onChangeText={(text) => setExcersizeDesription(text)}
+      value={excersizeDescription}
+      />
+    </KeyboardAvoidingView>
   )
 }
 
 const styles = StyleSheet.create({
-  bruh: {
-    borderRadius: 5,
-    borderColor: 'white',
+  textInput: {
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 14,
+    paddingRight: 14,
+    fontSize: 16,
+    color: 'black',
     backgroundColor: 'white',
-    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    fontSize: 24,
   },
-  bottom: {
-    marginTop: 500,
+  excersizeInput: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 14,
+    paddingRight: 14,
+    fontSize: 16,
+    color: 'black',
+    backgroundColor: 'white',
+    width: '100%',
+    alignItems: 'center',
+    fontSize: 24,
+
   }
 })
 
