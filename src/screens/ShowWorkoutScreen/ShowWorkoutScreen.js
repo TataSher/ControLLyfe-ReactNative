864f594ec70b286476b31530e73cc4d86d3c8f03
Emmapr123 <emmapriester@hotmail.com>
2021-05-13 17:15:30 +0100

    Styled exercises screen

--- src/screens/ShowWorkoutScreen/ShowWorkoutScreen.js
+++ src/screens/ShowWorkoutScreen/ShowWorkoutScreen.js
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TouchableHighlightBase, View } from 'react-native';
 
   const ExerciseList = (props) => {
     const { exercises = [], getWorkout } = props
     console.log(exercises)
     var titles = [];
     exercises.forEach((exercise) => {
-      titles.push(exercise.title)
+      titles.push({'title': exercise.title, 'duration': exercise.duration, 'description': exercise.description })
     });
+
     return titles.map((title, index) => {
-      return <Text key={index}>{title}</Text>
+       const minutes = Math.floor(title.duration / 60)
+       const seconds = title.duration - (minutes * 60) >= 0 ? title.duration - minutes * 60 : title.duration
+       const minutesDisplay = minutes < 10 ? `0${minutes}` : minutes
+       const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds
+
+      return <View key={index} style={styles.exerciseBox}>
+        <View style={styles.exerciseHeader}>
+      <Text style={styles.excerciseText}> {title.title} </Text>
+      <Text style={styles.excerciseText}> { minutesDisplay } : { secondsDisplay } </Text>
+      </View>
+      <Text style={styles.exerciseDescription}> {title.description}  </Text>
+      </View>
     });
   };
 
@@ -35,19 +47,44 @@ const ShowWorkoutScreen = (props) => {
   // Need to create an ExerciseListComponent - same as WorkoutListComponent
 
   return (
-    <View>
-      <Text>{workoutTitle}</Text>
+    <ScrollView>
+      <Text style={styles.workoutTitle}>{workoutTitle}</Text>
 
       <ScrollView>
         <ExerciseList exercises={exercises} />
       </ScrollView>
       
-    </View>
+    </ScrollView>
   );
 }
 
-
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  workoutTitle: {
+    color: 'gray',
+    fontSize: 32,
+    marginTop: 16,
+    marginBottom: 16,
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  exerciseBox: {
+    margin: 20,
+    backgroundColor: 'white'
+  },
+  exerciseHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  excerciseText: {
+    color: 'darkgray',
+    fontSize: 24,
+    margin: 8,
+  },
+  exerciseDescription: {
+    color: 'gray',
+    fontSize: 20,
+    margin: 8,
+  }
+})
 
 export { ShowWorkoutScreen }
