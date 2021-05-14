c84310ed77e2d1f8a67c9b6721d84ebc5f9665ad
Natalia_Sherchenkova <tataterer@gmail.com>
2021-05-14 15:22:33 +0100

    created an SVG StartWorkoutButton and started working on UseWorkoutScreen

---
+++ src/screens/UseWorkoutScreen/UseWorkoutScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Button, SafeAreaView, ScrollView, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+import { WorkoutTitleComponent } from '../../components';
+
+const UseWorkoutScreen = () => {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export { UseWorkoutScreen }
