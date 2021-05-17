a38f8b3bb0f8a884b228600426d6c1b16125fb4d
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-05-17 16:45:06 +0100

    Refactor still has a couple bugs and more to do

--- src/screens/AddNewWorkoutScreen/AddNewWorkoutScreen.js
+++ src/screens/AddNewWorkoutScreen/AddNewWorkoutScreen.js
@@ -45,6 +45,7 @@ const AddNewWorkoutScreen = ( {navigation, route} ) => {
   const [minutes, setMinutes] = useState(0);
   const [exercizes, setExercizes] = useState([]);
   const [exercizeIndex, setExercizeIndex] = useState(false);
+  // When images are working
   const [image, setImage] = useState('');
 
   useEffect(() =>{
@@ -101,54 +102,16 @@ const AddNewWorkoutScreen = ( {navigation, route} ) => {
   return(
     <KeyboardAvoidingView style={{flex: 1, position: 'relative'}}>
       {/* Start OF COMP */}
-    <View>
-      <TextInput
-        placeholder="Workout title"
-        style={styles.textInput}
-        onChangeText={(text) => setWorkoutTitle(text)}
-        value={workoutTitle}
-      />
-      <Text style={{fontSize: 24, margin: 8}}> Exercises </Text>
-    <View style={styles.exercizeInput}>
-      <TextInput
-      placeholder="Exercise name"
-      style={{fontSize: 24}}
-      onChangeText={(text) => setExercizeTitle(text)}
-      value={exercizeTitle}
-      />
-      <View style={{flexDirection: 'row', width: '50%'}}>
-      <Picker
-        style={{backgroundColor: 'white', flex: 1}}
-        selectedValue={minutes}
-        onValueChange={(itemValue) =>
-          setMinutes(itemValue)
-        }>
-            {minutesArray.map((minute) => {
-             return <Picker.Item key={minute} label={`${minute}`} value={minute} />
-            })}
-      </Picker>
-      <Picker
-        style={{backgroundColor: 'white', flex: 1}}
-        selectedValue={seconds}
-        onValueChange={(itemValue) =>
-          setSeconds(itemValue)
-        }>
-          {secondsArray.map((second) => {
-             return <Picker.Item key={second} label={`${second}`} value={second} />
-            })}
-      </Picker>
-      </View>
-      </View>
       <View>
-      <TextInput
-        placeholder='description'
-        style={styles.textInput}
-        onChangeText={(text) => setExercizeDescription(text)}
-        value={exercizeDescription}
+      <AddExercizeComponent 
+        setWorkoutTitle={setWorkoutTitle}
+        setExercizeTitle={setExercizeTitle}
+        setExercizeDescription={setExercizeDescription}
+        setSeconds={setSeconds}
+        setMinutes={setMinutes}
       />
-      {/* END OF COMP */}
       {editButton()}
-      </View>     
+
     </View>
     <ScrollView>
       <EditExercizeComponent 
@@ -190,32 +153,8 @@ const AddNewWorkoutScreen = ( {navigation, route} ) => {
 }
 
 const styles = StyleSheet.create({
-  textInput: {
-    paddingTop: 15,
-    paddingBottom: 15,
-    paddingLeft: 14,
-    paddingRight: 14,
-    fontSize: 16,
-    color: 'black',
-    backgroundColor: 'white',
-    width: '100%',
-    alignItems: 'center',
-    fontSize: 24,
-  },
-  exercizeInput: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingTop: 15,
-    paddingBottom: 15,
-    paddingLeft: 14,
-    paddingRight: 14,
-    fontSize: 16,
-    color: 'black',
-    backgroundColor: 'white',
-    width: '100%',
-    alignItems: 'center',
-    fontSize: 24,
-  },
+  
+  
   button: {
     position: 'absolute', 
     bottom: 40, left: 40, 
