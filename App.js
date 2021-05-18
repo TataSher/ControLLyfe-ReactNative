ed1866aef97e21b2156ee9327d47e6105bdab77e
Emmapr123 <emmapriester@hotmail.com>
2021-05-18 10:35:10 +0100

    Signup working

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { WorkoutListScreen, AddNewWorkoutScreen, ShowWorkoutScreen, StartWorkoutScreen } from './src/screens';
+import { WorkoutListScreen, AddNewWorkoutScreen, ShowWorkoutScreen, StartWorkoutScreen, SignUpScreen, SignInScreen } from './src/screens';
 
 const Stack = createStackNavigator();
 
@@ -11,6 +11,12 @@ export default function App() {
   return (
     <NavigationContainer >
       <Stack.Navigator  screenOptions={{headerTintColor: 'white', headerStyle: {backgroundColor: 'pink'}, headerBackTitle: "", headerTitle: 'ControLLyfe'}}>
+        <Stack.Screen name='Sign Up Screen'>
+          {props => <SignUpScreen {...props}/>}
+        </Stack.Screen>
+        <Stack.Screen name='Sign In Screen'>
+          {props => <SignInScreen {...props}/>}
+        </Stack.Screen>
         <Stack.Screen name="Workout List">
         {props => <WorkoutListScreen {...props}/>}
           </Stack.Screen>
