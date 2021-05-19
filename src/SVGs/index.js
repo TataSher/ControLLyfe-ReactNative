00d25f9799dc7eb39147306fb3cd48a223f010fe
Natalia_Sherchenkova <tataterer@gmail.com>
2021-05-19 14:34:50 +0100

    added Emmas Home button to EndWorkoutScreen

--- src/SVGs/index.js
+++ src/SVGs/index.js
@@ -4,3 +4,4 @@ export * from './SaveButton';
 export * from './EditWorkoutButton';
 export * from './DeleteWorkoutButton';
 export * from './StartWorkoutButton';
+export * from './HomeButton';
--- src/screens/EndWorkoutScreen/EndWorkoutScreen.js
+++ src/screens/EndWorkoutScreen/EndWorkoutScreen.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import { WorkoutTitleComponent } from '../../components';
+import { HomeButton } from '../../SVGs'
 
 const EndWorkoutScreen = ( props ) => {
 const workout = props.route.params
 const id = props.route.params.id
 
   return(
-    <View>
+    <View style={{flex: 1, position: 'relative'}}>
       <View style={styles.workoutTitle}>
         <WorkoutTitleComponent key={id} {...workout}/>
       </View>
@@ -15,6 +16,9 @@ const id = props.route.params.id
       <Text style={styles.endWorkoutText}>Workout Completed!</Text>
       <Text style={styles.endWorkoutText}>Well Done!</Text>
     </View>
+    <TouchableOpacity style={styles.HomeButton} onPress={() => props.navigation.navigate('Workout List')}>
+        <HomeButton color={'darkgray'} />
+      </TouchableOpacity>
    </View>
   )
 }
@@ -32,6 +36,14 @@ const styles = StyleSheet.create({
     color: 'pink',
     fontSize: 24,
     margin: 16
+  },
+  HomeButton: {
+    position: 'absolute', 
+    bottom: 50, right:30, 
+    shadowColor:'black', 
+    shadowRadius: 8, 
+    shadowOffset: {width: 5, height: 5}, 
+    shadowOpacity: 0.3
   }
 })
 
