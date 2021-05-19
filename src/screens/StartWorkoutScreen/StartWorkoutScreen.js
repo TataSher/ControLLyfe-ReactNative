29e46b3c0f898467659c63c23ac55519d0ec6944
Emmapr123 <emmapriester@hotmail.com>
2021-05-19 12:10:49 +0100

    Animated bits in separate components

--- src/screens/StartWorkoutScreen/StartWorkoutScreen.js
+++ src/screens/StartWorkoutScreen/StartWorkoutScreen.js
@@ -4,22 +4,103 @@ import { Vibration, StyleSheet, TextInput, Dimensions, View, Animated, Touchable
 import { WorkoutTitleComponent, IndividualExerciseComponent } from '../../components';
 import { StartWorkoutButton } from '../../SVGs';
 
-const StartWorkoutScreen = ( props ) => {
-  const workout = props.route.params
-  const exercises = props.route.params.exercises
-  const id = props.route.params.id
-
+const AnimatedViewBar = ( props ) => {
+  const { active, duration, onAnimationComplete } = props
   const { width, height } = Dimensions.get('window')
-
-  const ITEM_SIZE = width;
-  const ITEM_SPACING = (width - ITEM_SIZE);
-  const [currentIndex, setCurrentIndex] = useState(0)
-
-  const [duration, setDuration] = useState(exercises[currentIndex].duration);
+  const timerAnimation = React.useRef(new Animated.Value(0)).current;
   const inputRef = React.useRef();
-  const timerAnimation = React.useRef(new Animated.Value(height)).current;
-  const textInputAnimation = React.useRef(new Animated.Value(exercises[currentIndex].duration)).current;
-  const scrollerAnimation = React.useRef(new Animated.Value(exercises[currentIndex].duration)).current;
+  // const textInputAnimation = React.useRef(new Animated.Value(duration)).current;
+
+  React.useEffect(() => {
+    const listener = timerAnimation.addListener(({value}) => {
+      console.log('duration', duration)
+      console.log('value', value)
+      inputRef?.current?.setNativeProps({
+        text: Math.ceil(value * (1 - duration)).toString()
+      })
+    })
+
+    return () => {
+      timerAnimation.removeListener(listener)
+      timerAnimation.removeAllListeners();
+    }
+  })
+
+  // console.log('props', props)
+
+  const countDown = timerAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [duration, 0]
+  })
+
+  const animation = () => {
+
+    // Animated.sequence([
+
+      // Animated.timing(timerAnimation, {
+      //   toValue: 0,
+      //   duration: 300,
+      //   useNativeDriver: true
+      // }),
+      Animated.timing(timerAnimation, {
+        toValue: 1,
+        duration: duration * 1000,
+        useNativeDriver: true
+      }).start(() => {
+        console.log('here')
+        onAnimationComplete()
+      })
+    // ])
+  }
+
+  const translate = timerAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, height]
+    })
+
+  console.log(timerAnimation)
+
+  React.useEffect(() => {
+    if (active) {
+      animation()
+    }
+  }, [active])
+
+  return(
+    <View style={{fles: 1}}>
+    <Animated.View 
+        style={ {
+          position: 'absolute',
+          top: 0, left: 0, right: 0, bottom: 20,
+          width,
+          backgroundColor: 'red',
+          transform: [{
+          translateY: translate}]
+        }
+        }/>
+         <View style={{
+            postition: 'absolute',
+            justifyContent: 'center',
+            width: width,
+            alignSelf: 'center',
+            alignItems: 'center'
+          }}>
+            <TextInput 
+              ref={inputRef}
+              style={styles.duration}
+              defaultValue={duration.toString()}
+            />
+          </View>
+        </View>
+  )
+}
+
+const AnimatedCountDownComponent = ( props ) => {
+  const { duration, active } = props
+  const inputRef = React.useRef();
+  const { width } = Dimensions.get('window')
+  const ITEM_SIZE = width;
+  const textInputAnimation = React.useRef(new Animated.Value(duration)).current;
 
   React.useEffect(() => {
     const listener = textInputAnimation.addListener(({value}) => {
@@ -36,43 +117,109 @@ const StartWorkoutScreen = ( props ) => {
 
   const animation = React.useCallback(() => {
 
-    Animated.sequence([
+    Animated.timing(textInputAnimation, {
+      toValue: 0,
+      duration: duration * 1000,
+      useNativeDriver: true
+    })
+  }, [duration])
 
-      Animated.timing(timerAnimation, {
-        toValue: 0,
-        duration: 300,
-        useNativeDriver: true
-      }),
+  useEffect(() => {
+    if (active) {
+      animation()
+    }
+  }, [active])
 
-      Animated.parallel([
-        Animated.timing(textInputAnimation, {
-          toValue: 0,
-          duration: duration * 1000,
-          useNativeDriver: true
-        }),
-        Animated.timing(timerAnimation, {
-          toValue: height,
-          duration: duration * 1000,
-          useNativeDriver: true
-        }),
-        Animated.timing(scrollerAnimation, {
-          toValue: currentIndex,
-          duration: duration * 1000,
-          useNativeDriver: true
-        })
-      ]),
-      Animated.delay(300)
-      ]).start(() => {
-        Vibration.cancel();
-        Vibration.vibrate();
-          flatListRef.scrollToIndex({animated: true, index: currentIndex + 1})
-          textInputAnimation.setValue(duration);
-      }) 
-    }, [duration])
+  return (
+  <Animated.View style={{
+    postition: 'absolute',
+    justifyContent: 'center',
+    width: ITEM_SIZE,
+    alignSelf: 'center',
+    alignItems: 'center'
+  }}>
+    <TextInput 
+      ref={inputRef}
+      style={styles.duration}
+      defaultValue={duration.toString()}
+    />
+  </Animated.View>
+  )
+}
+
+const StartWorkoutScreen = ( props ) => {
+  const workout = props.route.params
+  const exercises = props.route.params.exercises
+  const id = props.route.params.id
+
+  const { width, height } = Dimensions.get('window')
+
+  const ITEM_SIZE = width;
+  const ITEM_SPACING = (width - ITEM_SIZE);
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const [duration, setDuration] = useState(exercises[currentIndex].duration);
+  const inputRef = React.useRef();
+  const timerAnimation = React.useRef(new Animated.Value(height)).current;
+  const textInputAnimation = React.useRef(new Animated.Value(exercises[currentIndex].duration)).current;
+  const scrollerAnimation = React.useRef(new Animated.Value(exercises[currentIndex].duration)).current;
+  const [animating, setAnimating] = useState(false)
+
+  // React.useEffect(() => {
+  //   const listener = textInputAnimation.addListener(({value}) => {
+  //     inputRef?.current?.setNativeProps({
+  //       text: Math.ceil(value).toString()
+  //     })
+  //   })
+
+  //   return () => {
+  //     textInputAnimation.removeListener(listener)
+  //     textInputAnimation.removeAllListeners();
+  //   }
+  // })
+
+  // const animation = React.useCallback(() => {
+
+  //   Animated.sequence([
+
+      // Animated.timing(timerAnimation, {
+      //   toValue: 0,
+      //   duration: 300,
+      //   useNativeDriver: true
+      // }),
+
+      // Animated.parallel([
+        // Animated.timing(textInputAnimation, {
+        //   toValue: 0,
+        //   duration: duration * 1000,
+        //   useNativeDriver: true
+        // }),
+        // Animated.timing(timerAnimation, {
+        //   toValue: height,
+        //   duration: duration * 1000,
+        //   useNativeDriver: true
+        // }),
+        // Animated.timing(scrollerAnimation, {
+        //   toValue: currentIndex,
+        //   duration: duration * 1000,
+        //   useNativeDriver: true
+        // })
+      // ]),
+      // Animated.delay(300)
+      // ]).start(() => {
+      //   Vibration.cancel();
+      //   Vibration.vibrate();
+          // textInputAnimation.setValue(duration);
+    //   }) 
+    // }, [duration])
+
+    const onAnimationComplete = () => {
+      flatListRef.scrollToIndex({animated: true, index: currentIndex + 1})
+    }
 
   return(
     <View style={{flex: 1}}>
-       <Animated.View 
+       {/* <Animated.View 
         style={[StyleSheet.absoluteFillObject, {
           height,
           width,
@@ -81,10 +228,13 @@ const StartWorkoutScreen = ( props ) => {
           translateY: timerAnimation}]
         }]
 
-        }/>
+        }/> */}
       <View style={styles.workoutTitle}>
         <WorkoutTitleComponent key={id} {...workout}/>
       </View>
+      <TouchableOpacity onPress={() => setAnimating(true)}>
+          <StartWorkoutButton/>
+        </TouchableOpacity>
       <View style={styles.flatList}>
         <Animated.FlatList 
         data={exercises}
@@ -99,22 +249,23 @@ const StartWorkoutScreen = ( props ) => {
         ref={(ref) => { flatListRef = ref; }}
         showsHorizontalScrollIndicator={false}
         snapToInterval={ITEM_SIZE}
-        style={StyleSheet.absoluteFillObject, {
-          height: 400,
-          width,
-          backgroundColor: 'blue',
-        }}
+        // style={StyleSheet.absoluteFillObject, {
+        //   height: 400,
+        //   width,
+        // }}
         contentContainerStyle={{
           paddingHorizontal: ITEM_SPACING,
         }}
-        renderItem={({item}) => {
+        renderItem={({item, index}) => {
 
-          return <View style={{width: ITEM_SIZE, alignItems: 'center', justifyContent: 'center', height: '100%'}}>
+          return <View style={{width: ITEM_SIZE, alignItems: 'center', justifyContent: 'center', height: height, position: 'relative'}}>
+          <AnimatedViewBar active={index === currentIndex && animating} {...item} {...{onAnimationComplete}}/> 
           <IndividualExerciseComponent exercise={item} />
+          {/* <AnimatedCountDownComponent duration={item.duration} active={index === currentIndex && animating}/> */}
         </View>
         }}
         />
-        <Animated.View style={{
+        {/* <Animated.View style={{
           postition: 'absolute',
           justifyContent: 'center',
           width: ITEM_SIZE,
@@ -125,11 +276,8 @@ const StartWorkoutScreen = ( props ) => {
             ref={inputRef}
             style={styles.duration}
             defaultValue={duration.toString()}
-          />
-        </Animated.View>
-        <TouchableOpacity onPress={animation}>
-          <StartWorkoutButton/>
-        </TouchableOpacity>
+          /> */}
+        {/* </Animated.View> */}
       </View>
     </View>
   )
